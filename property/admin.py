6cba263a289356c73dcfd4e93a8899ec6d9901be
Nikint86 <lobackiy_2007@mail.ru>
2026-03-11 12:36:00 +0300

    Customize Flat Admin.

--- property/admin.py
+++ property/admin.py
@@ -2,8 +2,31 @@ from django.contrib import admin
 from .models import Flat
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['city', 'street', 'address']
+    search_fields = ['town', 'town_district', 'address']
 
+    readonly_fields = ['created_at']
 
-admin.site.register(Flat, FlatAdmin)
+    fieldsets = (
+        ('Владелец', {
+            'fields': ('owner', 'owners_phonenumber')
+        }),
+        ('Адрес', {
+            'fields': ('town', 'town_district', 'address', 'floor')
+        }),
+        ('Характеристики', {
+            'fields': ('price', 'rooms_number', 'living_area', 'has_balcony', 'construction_year')
+        }),
+        ('Описание', {
+            'fields': ('description',)
+        }),
+        ('Служебная информация', {
+            'fields': ('created_at', 'active'),
+            'classes': ('wide',)
+        }),
+    )
+
+    list_display = ('address', 'town', 'price', 'rooms_number', 'active', 'created_at')
+
+    list_filter = ('active', 'town', 'rooms_number', 'has_balcony')
